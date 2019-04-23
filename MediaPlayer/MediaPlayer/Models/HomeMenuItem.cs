7a255fbd07b8557417ce4b9652468317eb78c8a9
Gurpreet Deol <deol.gs@hotmail.com>
2019-04-23 13:15:34 -0700

    MediaPlayer mobile app initial setup on 2019-04-23 at 01:15 PM

---
+++ MediaPlayer/MediaPlayer/Models/HomeMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaPlayer.Models
+{
+    public enum MenuItemType
+    {
+        Browse=1,
+        About=2,
+        AudioPlayer=3
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
